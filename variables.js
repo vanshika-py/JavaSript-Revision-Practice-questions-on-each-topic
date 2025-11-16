d94c0291e66516cf0853cfc69a8f2e7fdec7d969
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2025-11-16 17:43:12 +0530

    started revision and done 3 basic question

---
+++ variables.js
@@ -0,0 +1,26 @@
+//1.Declare a variable to store your name and print it.
+
+
+/*let name = "Vanshika kathpal";
+console.log("my name = " + name);*/
+
+
+//2.What happens if you reassign a const variable? Try it. 
+
+/*exp : it will return error*/
+
+/*const studentName = "Diya Kathpal";
+studentName = "Vansh";
+console.log(studentName);*/
+
+//3.Create 3 variables: one number, one string, one boolean. Log their types.
+
+/*let num = 57;
+console.log(typeof num);*/
+
+/*let str = "I am learning JavaScript";
+console.log(typeof str);
+
+let ismarried = true;
+console.log(typeof ismarried);
+
