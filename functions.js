fec21e9d597792dc34198f86f8c0707279944bb2
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-01-11 00:56:28 +0530

    practiced 4 questions on functions

---
+++ functions.js
@@ -0,0 +1,44 @@
+/*Beginner:
+1.create a function which adds who numbers.*/
+
+/*function sum(a,b){
+    console.log(a+b)
+}
+
+sum(4,5)*/
+
+//2. Write a function that returns the square of a number//
+
+/*function squa(a){
+    console.log(a*a)
+}
+
+//squa(5)//
+
+squa(12);*/
+
+//3. Make a function that prints "Hello JavaScript".//
+
+ /*let greet = () => {
+    console.log("Hello JavaScript")
+}
+
+greet();*/
+
+//4. Create a function that checks if a number is odd or even//
+
+/*function check(a){
+    if(a%2 === 0){
+    console.log("even")
+    } else {
+        console.log("odd")
+    }
+}
+
+//check(25);//
+
+//check(6);//
+
+
+//5. Convert a normal function to an arrow function//
+
